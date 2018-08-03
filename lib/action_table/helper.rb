a8ba984e75917602313fb816df5de5cfdd2f0b39
Jacob Burenstam <burenstam@gmail.com>
2018-08-03 01:40:56 +0100

    Add Helper#action_table docs

--- lib/action_table/helper.rb
+++ lib/action_table/helper.rb
@@ -5,6 +5,20 @@ require 'action_table/view'
 
 module ActionTable
   module Helper
+    # Renders an ActiveRecord collection as a HTML table.
+    # @return [String] returns a HTML string representing the ActiveRecord collection.
+    # @param [ActiveRecord::Collection]
+    #   records ActiveRecord collection to render
+    # @param [Array<String>, Array<Symbol>]
+    #   table columns (must map to methods on each ActiveRecord instance)
+    # @param styles [Array<String>, Array<Symbol>]
+    #   no, one or many of bootstrap table styles (table- prefix will be added)
+    # @param link [Symbol]
+    #   method name for the default name to use for anchor-tags
+    # @param paginate [true, false]
+    #   whether to render pagination links (default: false)
+    # @param actions [Array<String>, Array<Symbol>]
+    #   render action lins (show, edit and delete)
     def action_table(
       records,
       fields,
